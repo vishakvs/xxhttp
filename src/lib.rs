2dd98f3a64097599732db6ec6d5c0090d1621353
Vishak V S <vsvishak9@gmail.com>
2022-05-27 03:19:51 +0530

    just few lines test

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,28 @@
+//! Fast, minimalist web framework for Rust
+
+/// Describes the xxhttp server
+pub struct Server {
+    ipv4addr: u32,
+}
+
+impl Server {
+    /// Get bounded IP of Server instance
+    pub fn getip(&self) -> u32 {
+        self.ipv4addr
+    }
+}
+
+/// Returns the instance of Server.
+pub fn xxhttp_server() -> Server {
+    let s = Server { ipv4addr: 1 };
+    s
+}
+
 #[cfg(test)]
 mod tests {
+    //use super::*;
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(1, 1);
     }
 }
